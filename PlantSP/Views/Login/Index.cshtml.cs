2c0cbe41afc6fc29557525f696cbb405396b1409
Luis Henrique <duimbispo@gmail.com>
2024-06-11 00:34:29 -0300

    Teste

---
+++ PlantSP/Views/Login/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace PlantSP.Views.Shared.Login
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
